87315150c1000f08dfe669f016fade7290775031
66070063 <66070063@kmitl.ac.th>
2025-09-05 23:06:10 +0700

    add component admin hotel

---
+++ src/client/components/AdminHotel.vue
@@ -0,0 +1,36 @@
+<script setup>
+    const ImageTest = `/src/client/assets/images/test.png`
+</script>
+
+<template>
+    <div class="m-16">
+        <div class="grid grid-cols-3 m-auto w-300">
+            <div class="p-6 w-90 rounded-md shadow-2xl">
+                <img :src="ImageTest" alt="test-hotel-image" class="w-75 h-45 object-cover m-auto rounded-md">
+                <div class="text-center p-4 font-bold text-[18px]">โรงแรมดาวมีไว้เบิ่ง 1 | กรุงเทพ</div>
+
+                <div class="flex justify-end">
+                    <button class="text-white bg-[#102B58] text-[14px] font-semibold p-2.5 cursor-pointer rounded-sm">แก้ไขรายละเอียด</button>
+                </div>
+            </div>
+
+            <div class="p-6 w-90 rounded-md shadow-2xl">
+                <img :src="ImageTest" alt="test-hotel-image" class="w-80 h-45 object-cover m-auto rounded-md">
+                <div class="text-center p-4 font-bold text-[18px]">โรงแรมดาวมีไว้เบิ่ง 1 | กรุงเทพ</div>
+                
+                <div class="flex justify-end">
+                    <button class="text-white bg-[#102B58] text-[14px] font-semibold p-2.5 cursor-pointer rounded-sm">แก้ไขรายละเอียด</button>
+                </div>
+            </div>
+
+            <div class="p-6 w-90 rounded-md shadow-2xl">
+                <img :src="ImageTest" alt="test-hotel-image" class="w-80 h-45 object-cover m-auto rounded-md">
+                <div class="text-center p-4 font-bold text-[18px]">โรงแรมดาวมีไว้เบิ่ง 1 | กรุงเทพ</div>
+                
+                <div class="flex justify-end">
+                    <button class="text-white bg-[#102B58] text-[14px] font-semibold p-2.5 cursor-pointer rounded-sm">แก้ไขรายละเอียด</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
