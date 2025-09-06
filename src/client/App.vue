e7bf03ffa3386f2118d02d2c1a8ae84c133b9e0d
Serista3 <stacla5282@gmail.com>
2025-09-06 16:06:38 +0700

    fix fetch user

--- src/client/App.vue
+++ src/client/App.vue
@@ -2,24 +2,33 @@
 import Navbar from './components/Navigation.vue'
 import Footer from './components/Footer.vue';
 import { useRoute } from 'vue-router';
-import { computed, onMounted, ref } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import axios from 'axios';
 
-const routes = useRoute()
+const route = useRoute();
 
 const showNav = computed(() => {
-  return routes.path === '/login' || routes.path === '/register' ? false : true;
-})
+  return route.path !== '/login' && route.path !== '/register';
+});
 
-const userNow = ref(null)
+const userNow = ref(null);
 
-onMounted(async () => {
+const fetchUserCurrent = async () => {
   try {
     const resUsers = await axios.get('http://localhost:3000/api/users');
     const resUserNow = await axios.get(`http://localhost:3000/api/profile`, { withCredentials: true });
     userNow.value = Array.from(resUsers.data).filter(user => user.user_id === Number(resUserNow.data.userId))[0];
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error fetching user:', error);
+    userNow.value = null;
+  }
+};
+
+onMounted(fetchUserCurrent);
+
+watch(() => route.path, (newPath) => {
+  if (newPath !== '/login' && newPath !== '/register') {
+    fetchUser();
   }
 });
 
