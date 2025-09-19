454c58074ef35729a9ff6c65bad7f2ad0aca174b
66070063 <66070063@kmitl.ac.th>
2025-09-19 14:59:30 +0700

    format date month year

--- src/views/components/HotelDetails_History.vue
+++ src/views/components/HotelDetails_History.vue
@@ -4,6 +4,7 @@ const Amount = `/src/views/assets/icons/amount-room.png`
 const Price = `/src/views/assets/icons/price.png`
 const ImageTest = `/src/views/assets/images/test.png`
 import { ref } from 'vue';
+import dayjs from 'dayjs';
 
 const props = defineProps({
     booking: {
@@ -14,13 +15,40 @@ const props = defineProps({
 
 const booking = ref({ ...props.booking });
 
+const ThaiMonth = [
+    'มกราคม',
+    'กุมภาพันธ์',
+    'มีนาคม',
+    'เมษายน',
+    'พฤษภาคม',
+    'มิถุนายน',
+    'กรกฎาคม',
+    'สิงหาคม',
+    'กันยายน',
+    'ตุลาคม',
+    'พฤศจิกายน',
+    'ธันวาคม'
+]
+
+const checkIn = new Date(booking.value.check_in_date)
+const checkOut = new Date(booking.value.check_out_date)
+
+const checkInDay = dayjs(checkIn).date()
+const checkOutDay = dayjs(checkOut).date()
+
+const checkInMonth = ThaiMonth[dayjs(checkIn).month()]
+const checkOutMonth = ThaiMonth[dayjs(checkOut).month()]
+
+const checkInYear = dayjs(checkIn).year() + 543
+const checkOutYear = dayjs(checkOut).year() + 543
+
 </script>
 
 <template>
     <div class="grid m-10 justify-center">
         <div class="flex shadow-2xl rounded-lg p-4">
             <img :src="ImageTest" alt="test-hotel-image" class="w-50 h-50 object-cover rounded-md">
-            <div class="grid p-8 pt-0 min-w-250">
+            <div class="grid p-8 pt-0 w-250">
                 <!-- ชื่อโรงแรม -->
                 <div class="font-bold text-[24px]">
                     <span>{{ booking.hotel_name }}</span>
@@ -28,7 +56,7 @@ const booking = ref({ ...props.booking });
                 <!-- วันที่จองเข้า - ออก -->
                 <div class="inline-flex gap-1.5">
                     <img :src="Calendar" alt="calendar" class="w-6 h-6">
-                    <span class="">{{ booking.check_in_date }} - {{ booking.check_out_date }}</span>
+                    <span class="">{{ checkInDay }} {{ checkInMonth }} {{ checkInYear }} - {{ checkOutDay }} {{ checkOutMonth }} {{ checkOutYear }}</span>
                 </div>
                 <!-- จำนวนห้องที่พัก/คืน -->
                 <div class="inline-flex gap-1.5">
@@ -43,7 +71,6 @@ const booking = ref({ ...props.booking });
                     <span class="">{{ booking.total_price }}</span>
                 </div>
             </div>
-            <!-- สถานะ: สำเร็จ -->
             <div class="flex items-end mr-4">
                 <div class="p-2.5 text-center w-30 text-white rounded-sm font-semibold" 
                     :class="{
@@ -55,10 +82,6 @@ const booking = ref({ ...props.booking });
                     {{ booking.booking_status }}
                 </div>
             </div>
-            <!-- สถานะ: กำลังรอ -->
-            <!-- <div class="flex items-end mr-4">
-                <div class="bg-[#F2B900] p-2.5 text-center w-30 text-white rounded-sm font-semibold">กำลังรอ</div>
-            </div> -->
         </div>
     </div>
 </template>
