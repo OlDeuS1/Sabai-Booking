94c678ad0b9687b1193108b019357a5c04ae4504
Serista3 <stacla5282@gmail.com>
2025-10-23 19:08:33 +0700

    add sessionToken

--- src/server/aws/s3.js
+++ src/server/aws/s3.js
@@ -6,10 +6,11 @@ dotenv.config();
 const region = process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION || "ap-southeast-1";
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const sessionToken = process.env.AWS_SESSION_TOKEN;
 
 const s3 = new S3Client({
   region,
-  credentials: accessKeyId && secretAccessKey ? { accessKeyId, secretAccessKey } : undefined,
+  credentials: accessKeyId && secretAccessKey ? { accessKeyId, secretAccessKey, sessionToken } : undefined,
 });
 
 export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
