a4fe336a568f50109bae616ab740f2aa45c49754
Serista3 <stacla5282@gmail.com>
2025-10-24 19:21:34 +0700

    fix bug ui

--- src/views/views/AdminHotel.vue
+++ src/views/views/AdminHotel.vue
@@ -118,7 +118,7 @@ const handleEditHotel = (hotelId) => {
 
 <template>
   <div class="admin__hotel bg-[#102B58] text-white min-h-screen">
-      <div class="admin__hotel-container max-w-7xl mx-auto px-[32px] flex flex-col pt-8 pb-40 sticky top-0 left-0">
+      <div class="admin__hotel-container max-w-7xl mx-auto px-[32px] flex flex-col pt-8 pb-40">
           <div class="admin__hotel-header flex justify-between items-center">
             <h1 class="text-4xl font-semibold text-left mt-12 mb-12">My Hotel</h1>
             <div class="admin__hotel-button flex justify-center items-center gap-6">
